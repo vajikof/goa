174fe649327c330ab7de2f916895bc0f34771eeb
vajiko <vajikopapiashvili0@gmail.com>
2024-03-19 13:28:13 +0400

    commit

---
+++ day 13/homework/day13.py
@@ -0,0 +1,95 @@
+
+
+# 1
+age = int(input("your age: "))
+if age < 18:
+    print("You are a minor")
+elif age > 18 and age < 65:
+    print("You are an adult")
+elif age >= 65:
+    print("You are a senior citizen")
+else:
+    print("df j kjd fjed")
+
+# 2
+for i in range(5):
+    num = int(input("enter number: "))
+    if num % 2 == 0:
+        print(num,"ლუწია")
+    else:
+        print(num,"კენტია")
+
+
+# 3 
+grade = input("   :: ::::::")
+if grade == "A":
+    print("Excelent")
+elif grade == "B":
+    print("Good job")
+elif grade == "C":
+    print("You passed")
+elif grade == "D":
+    print("You can do better")
+elif grade == "F":
+    print("You failed")
+else:
+    print("invalid asos")
+
+
+# 4
+x = 0
+while x > 10:
+    print(x)
+    x +=1
+
+# 5
+num = 0
+while num < 1 or num > 10:
+    num = int(input("enter number of 1-10: "))
+print("sworia")# 1
+age = int(input("your age: "))
+if age < 18:
+    print("You are a minor")
+elif age > 18 and age < 65:
+    print("You are an adult")
+elif age >= 65:
+    print("You are a senior citizen")
+else:
+    print("df j kjd fjed")
+
+# 2
+for i in range(5):
+    num = int(input("enter number: "))
+    if num % 2 == 0:
+        print(num,"ლუწია")
+    else:
+        print(num,"კენტია")
+
+
+# 3 
+grade = input("   :: ::::::")
+if grade == "A":
+    print("Excelent")
+elif grade == "B":
+    print("Good job")
+elif grade == "C":
+    print("You passed")
+elif grade == "D":
+    print("You can do better")
+elif grade == "F":
+    print("You failed")
+else:
+    print("invalid asos")
+
+
+# 4
+x = 0
+while x > 10:
+    print(x)
+    x +=1
+
+# 5
+num = 0
+while num < 1 or num > 10:
+    num = int(input("enter number of 1-10: "))
+print("sworia")
