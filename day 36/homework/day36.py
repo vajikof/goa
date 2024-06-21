b41c306b861a70aa340014564a623dd613badf66
vajiko <vajikopapiashvili0@gmail.com>
2024-06-21 13:20:06 +0400

    commit

---
+++ day 36/homework/day36.py
@@ -0,0 +1,17 @@
+student = {
+    'name': 'John Doe',
+    'age': 20,
+    'grade': 'A',
+    'city': 'New York'
+}
+
+print(student) 
+
+result = []
+
+for value in student.values():
+    result.append(value)
+
+print(result)
+
+
